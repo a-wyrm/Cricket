bdcea94496a3d716ed2d55e4792e21efcb8b9942
a-wyrm <86126872+a-wyrm@users.noreply.github.com>
2021-07-14 22:51:19 -0400

    Add files via upload

---
+++ Cricket.ino
@@ -0,0 +1,14 @@
+// character array for every ASCII character
+byte braille_dots[255];
+void setup() {
+  // put your setup code here, to run once:
+  for (int i = 0; i <= 255; i = i + 1) {
+    braille_dots[i] = 99;
+  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
